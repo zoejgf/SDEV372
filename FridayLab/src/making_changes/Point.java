b3b134be8b1c261386841c885c8c67982540c120
zoejgf <zoejgf@gmail.com>
2023-09-29 21:46:13 -0700

    added an equals() and hashcode() method

--- FridayLab/src/making_changes/Point.java
+++ FridayLab/src/making_changes/Point.java
@@ -1,5 +1,7 @@
 package making_changes;
 
+import java.util.Objects;
+
 public class Point
 {
     private double x;
@@ -24,6 +26,19 @@ public class Point
 
     public void setY(double y) {
         this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return Double.compare(x, point.x) == 0 && Double.compare(y, point.y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 
     @Override
