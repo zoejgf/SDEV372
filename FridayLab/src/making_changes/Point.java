42715f7352aa9f6b01ed6c17423d6b5ee090be6e
zoejgf <zoejgf@gmail.com>
2023-09-29 22:19:57 -0700

    Reverted to a previous version of my Point class. Committing changes to alter the (X, y) coordinate to am (x, y, z) coordinate.

--- FridayLab/src/making_changes/Point.java
+++ FridayLab/src/making_changes/Point.java
@@ -46,41 +46,4 @@ public class Point
                 ", z=" + z +
                 '}';
     }
-
-    public double getX() {
-        return x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Point point = (Point) o;
-        return Double.compare(x, point.x) == 0 && Double.compare(y, point.y) == 0;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(x, y);
-    }
-
-    @Override
-    public String toString() {
-        return "Point{" +
-                "x=" + x +
-                ", y=" + y +
-                '}';
-    }
 }
