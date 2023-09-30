fd885322585ef7763a20659631ae24f9b384b3b7
zoejgf <zoejgf@gmail.com>
2023-09-29 21:44:43 -0700

    added getters/setters and a totring() method to the Point class

--- FridayLab/src/making_changes/Point.java
+++ FridayLab/src/making_changes/Point.java
@@ -9,4 +9,28 @@ public class Point
         this.x = x;
         this.y = y;
     }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
 }
